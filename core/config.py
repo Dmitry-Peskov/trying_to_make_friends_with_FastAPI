bea877f738df044d2e606be3a9ec940e73367294
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-03-26 23:08:15 +0300

    Настроил Alembic для управления миграциями БД. Исправил ошибку в конфигурации для БД

--- core/config.py
+++ core/config.py
@@ -8,8 +8,8 @@ BASE_DIR = Path(__file__).parent.parent
 class SettingsDB(BaseSettings):
     dsn: str = f"sqlite+aiosqlite:///{BASE_DIR}/database.sqlite3"
     echo: bool = True
-    autoflush: bool = False,
-    autocommit: bool = False,
+    autoflush: bool = True
+    autocommit: bool = True
     expire_on_commit: bool = False
 
 
--- migrations/env.py
+++ migrations/env.py
@@ -20,12 +20,15 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from core.models import BaseModel
+from core.config import db_settings
+target_metadata = BaseModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option("sqlalchemy.url", db_settings.dsn)
 
 
 def run_migrations_offline() -> None:
