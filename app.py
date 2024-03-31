27bfa1035ab6d68015882c9eac082f4d9cc4e1b0
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-03-31 19:36:58 +0300

    Добавил описание API

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import uvicorn
 from api_v1 import api_v1_router
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Peskov-API",
+    version="0.1.0",
+    summary="Знакомлюсь с возможностями фреймворка FastAPI"
+)
 app.include_router(api_v1_router)
 
 
-@app.get("/")
+@app.get("/", summary="Стартовая страница")
 def index():
-    return {"status": 200, "message": "Welcome!"}
+    return {"status": 200, "message": "Добро пожаловать в Peskov-API"}
 
 
 if __name__ == "__main__":
